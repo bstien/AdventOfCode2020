4822051d388d665de25cf7dbb3c1ebfdce28134b
Bastian Stien <bastian.stien@gmail.com>
2020-12-05 12:54:20 +0100

    Day 5 - part 2

--- AdventOfCode2020/Days/Day5.swift
+++ AdventOfCode2020/Days/Day5.swift
@@ -10,11 +10,27 @@ struct Day5: Day {
     static func run(input: String) {
         let lines = splitInput(input)
         part1(lines: lines)
+        part2(lines: lines)
     }
 
     static func part1(lines: [String]) {
         let highestId = lines.map(parseSeat).map(\.seatId).max()
-        printResult(dayPart: 1, message: "Highest seat ID: \(highestId)")
+        printResult(dayPart: 1, message: "Highest seat ID: \(highestId!)")
+    }
+
+    static func part2(lines: [String]) {
+        let takenSeats = lines.map(parseSeat).map(\.seatId).sorted()
+
+        guard
+            let minId = takenSeats.first,
+            let maxId = takenSeats.last,
+            let mySeat = (minId...maxId).filter({ !takenSeats.contains($0) }).first
+        else {
+            printResult(result: .fail, dayPart: 2, message: "Could not find my seath ☹️")
+            return
+        }
+
+        printResult(dayPart: 2, message: "My seat ID: \(mySeat)")
     }
 }
 
