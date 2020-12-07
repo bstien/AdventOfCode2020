dd738449a5ee78cdb74c7875e600bfce9c4aa10a
Bastian Stien <bastian.stien@gmail.com>
2020-12-07 14:04:08 +0100

    Day 7 - part 2

--- AdventOfCode2020/Days/Day7.swift
+++ AdventOfCode2020/Days/Day7.swift
@@ -6,6 +6,7 @@ struct Day7: Day {
     static func run(input: String) {
         let bagRules = parseBags(from: input)
         part1(rules: bagRules)
+        part2(rules: bagRules)
     }
 
     private static func part1(rules: BagRules) {
@@ -29,6 +30,26 @@ struct Day7: Day {
 
         let result = rules.map(\.key).filter(containsShinyGoldBag)
         printResult(dayPart: 1, message: "Number of bags that can contain shiny gold: \(result.count)")
+    }
+
+    private static func part2(rules: BagRules) {
+        var checkedBags = [String: Int]()
+
+        func findNumberOfChildren(for bagName: String) -> Int {
+            if let existingResult = checkedBags[bagName] { return existingResult }
+
+            var bagCount = 1
+            if let children = rules[bagName], !children.isEmpty {
+                bagCount = children.map { $0.qty * findNumberOfChildren(for: $0.name) }.reduce(1, +)
+            }
+
+            checkedBags[bagName] = bagCount
+            return bagCount
+        }
+
+
+        let bagCount = findNumberOfChildren(for: "shiny gold") - 1
+        printResult(dayPart: 2, message: "Number of bags contained in shiny gold: \(bagCount)")
     }
 }
 
