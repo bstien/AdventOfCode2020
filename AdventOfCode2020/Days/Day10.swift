40a37dec37f4edc9cdada1152708d0ee29c4a229
Bastian Stien <bastian.stien@gmail.com>
2020-12-10 19:45:52 +0100

    Solution working for tests, but not for the provided input

--- AdventOfCode2020/Days/Day10.swift
+++ AdventOfCode2020/Days/Day10.swift
@@ -6,6 +6,7 @@ struct Day10: Day {
         adapters = adapters + [adapters.max()! + 3]
 
         part1(adapters: adapters)
+        part2(adapters: adapters)
     }
 
     static func part1(adapters: [Int]) {
@@ -20,5 +21,21 @@ struct Day10: Day {
 
         let result = joltDifference[1, default: 0] * joltDifference[3, default: 0]
         printResult(dayPart: 1, message: "Joltdifferences multiplied: \(result)")
+    }
+
+    static func part2(adapters: [Int]) {
+        func uniqueCombinations(currentAdapter: Int, adapters: [Int]) -> Int {
+            if adapters.count == 1 { return 1 }
+
+            let potentialAdapters = adapters.filter { (1...3).contains($0 - currentAdapter) }
+            if potentialAdapters.isEmpty { return 0 }
+
+            return potentialAdapters.map { adapter in
+                uniqueCombinations(currentAdapter: adapter, adapters: adapters.filter { $0 > adapter })
+            }.reduce(0, +)
+        }
+
+        let result = uniqueCombinations(currentAdapter: 0, adapters: adapters)
+        printResult(dayPart: 2, message: "Number of unique adapter combinations: \(result)")
     }
 }
